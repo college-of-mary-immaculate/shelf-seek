f4bc1b937024016f580c3430958c219c0ece9c01
Haimonmon <beankor8@gmail.com>
2025-09-28 22:06:57 +0800

    Added: limit params

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import time
 import snippy
 
 snippy.seek_checkup(headless = False)
 
-snippy.seek_openlibrary(headless = True, total_books = 150)
+snippy.seek_openlibrary(
+    online = False,
+    headless = True,
+    total_books = 150,
+    total_subject = 200,
+    total_tabs = 3
+)
