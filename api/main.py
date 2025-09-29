9c9b2ff75177d448bfd69d1733e36e681f09ba90
rey <reyarby.castro@gmail.com>
2025-09-29 16:12:40 +0800

    update: added search here

--- api/main.py
+++ api/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from routers import greeting_router
 from config import settings
+from ..model import SearchRequest, SearchResult, SearchResponse
 
 app = FastAPI(
     title=settings.APP_NAME, 
@@ -14,3 +15,7 @@ app.include_router(greeting_router, prefix=settings.API_PREFIX)
 @app.get("/")
 def root():
     return {"message": f"{settings.APP_NAME} is running"}
+
+@app.get("/search")          #, response_model=SearchResponse)
+def search():
+    return {"message": "Search Here!"}
