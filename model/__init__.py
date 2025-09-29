07bdeb642d2402d8761417b8cdecf8a2911935e4
rey <reyarby.castro@gmail.com>
2025-09-29 16:13:27 +0800

    update: for model import

---
+++ model/__init__.py
@@ -0,0 +1,8 @@
+from .searchEngineModel import (
+    ShelfSeekModel,
+    SearchRequest, 
+    SearchResult,
+    SearchResponse
+)
+
+__all__ = ["ShelfSeekModel", "SearchRequest", "SearchResult", "SearchResponse"]
