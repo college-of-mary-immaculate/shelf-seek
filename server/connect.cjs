e3846f4a6b3af229ab0793fb860479e496082a40
Rodien Jillian Ellorando <rodiengorg@gmail.com>
2025-09-29 23:38:11 +0800

    add MongoDB connection setup

---
+++ server/connect.cjs
@@ -0,0 +1,21 @@
+const { MongoClient } = require("mongodb")
+require("dotenv").config({ path: "./config.env" })
+
+async function main() {
+    const Db = process.env.ATLAS_URI
+    const client = new MongoClient(Db)
+
+    try {
+        await client.connect()
+        const collections = await client.db("shelfseek").collections()
+        collections.forEach((collection) =>
+            console.log(collection.collectionName)
+        )
+    } catch (e) {
+        console.error(e)
+    } finally {
+        await client.close()
+    }
+}
+
+main()
