71ae7c8931b762535974abcc3e18749a9039ba3d
rey <reyarby.castro@gmail.com>
2025-10-07 13:56:19 +0800

    update: fixed imports of collections

--- api/routers/__init__.py
+++ api/routers/__init__.py
@@ -1,11 +1,6 @@
 from .greeting import router
-
-__all__ = ["router"]
-from fastapi import APIRouter
-from .greeting import router
 from .list_collection import router as collection_router
 
-router = APIRouter()
+__all__ = ["router", "collection_router"]
 
-router.include_router(router, tags=["Greeting"])
-router.include_router(collection_router, prefix="/db", tags=["Database"])
+
