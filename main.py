daece99d70e04c861f78d40f11e7e267e3a18293
Haimonmon <beankor8@gmail.com>
2025-10-14 17:55:20 +0800

    Test: bookbrains classifier

--- main.py
+++ main.py
@@ -45,31 +45,16 @@ import bookbrains
 #     suggested += " "
 #     suggested += bookbrains.correct(token.lower(), choices=data)[0]
 
-# bookbrains.prepare_data()
+# bookbrains.prepare_data(force_rebuild = True)
 
-# queries = [
-#     "who wrote The Housemaid",                        # → author_name_search
-#     "summary of A Song to Drown Rivers",              # → book_content_search
-#     "tell me about Freida McFadden",                  # → author_content_search
-#     "books published by HarperCollins",               # → publisher_name_search
-#     "show me fantasy novels with mythology",          # → genre_name_search
-#     "what is The Let Them Theory about",              # → book_content_search
-#     "give me titles by Mel Robbins",                  # → author_name_search
-#     "list books from St Martins Publishing Group",    # → publisher_name_search
-#     "who is R. F. Kuang",                             # → author_name_search
-#     "describe the plot of The Irish Goodbye",         # → book_content_search
-#     "fiction about boarding schools or scandals",     # → genre_name_search
-#     "books by Dan Brown about secret societies",      # → author_name_search
-#     "which book is A Fine Line Between Stupid and Clever",  # → book_title_search
-#     "romance novels with small town settings",        # → genre_name_search
-#     "tell me more about author Brynne Weaver",        # → author_content_search
-#     "novels published by Zando",                      # → publisher_name_search
-#     "titles by Adalyn Grace",                         # → author_name_search
-#     "what is When the Cranes Fly South about",        # → book_content_search
-#     "books in the Dream Harbor series",               # → book_title_search
-#     "list business or motivation self-help books"     # → genre_name_search
-# ]
+queries = [
+    "Indie bestselling author of critically acclaimed",
+    "Her books have been sold in over twenty foreign territories",
+    "Book created by ann laing",
+    "books about mythology",
+    "show me books about fantasy"
+]
 
-
-# bookbrains.identify("list about business or motivation self help books")
+for query in queries:
+    bookbrains.identify(query, retrain = False)
     
