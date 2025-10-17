703c8058d5f65d26a321615ea373c9fb444c939d
Haimonmon <beankor8@gmail.com>
2025-10-18 03:32:35 +0800

    Fixing front

--- app/components/result/header.js
+++ app/components/result/header.js
@@ -2,65 +2,74 @@ import styles from './component.module.css';
 
 export default function Header(root) {
   root.innerHTML = `
-    <div class=${styles["content-container"]}>
-      <div class=${styles["searchbar-container"]}>
-        <div class=${styles["header-searchbar-container"]}>
-          <input type="text" id="header-searchbar-input" placeholder="Seek Books.">
-          <div class=${styles["searchbar-button-container"]}>
-            <img src="https://res.cloudinary.com/dhisbk3b2/image/upload/v1760196186/star_3_f0gvo8.png" alt="">
-          </div>
-
-          <div class=${styles["auto-suggest-container"]} id="auto-suggest">
-            <div class=${styles["suggest-item"]}>
-              <img src="https://res.cloudinary.com/dayv9oa8q/image/upload/v1760368039/bulb_1_vlovvz.png" alt="suggest-icon" class=${styles["suggest-icon"]}/>
-              <div class=${styles["suggest-text"]}>How to be a racist?</div>
+    <div class="${styles['header-wrapper']}">
+      <div class="${styles['logo-box']}">
+          <picture>
+              <source media="(min-width: 770px)" srcset="/img/desktop-logo.png" />
+              <source media="(max-width: 1024px)" srcset="/img/mobile-logo.png" />
+              <img src="/img/desktop-logo.png" alt="ShelfSeek Logo" />
+          </picture>
+      </div>
+      <div class=${styles["content-container"]}>
+        <div class=${styles["searchbar-container"]}>
+          <div class=${styles["header-searchbar-container"]}>
+            <input type="text" id="header-searchbar-input" placeholder="Seek Books.">
+            <div class=${styles["searchbar-button-container"]}>
+              <img src="https://res.cloudinary.com/dhisbk3b2/image/upload/v1760196186/star_3_f0gvo8.png" alt="">
             </div>
 
-            <div class=${styles["suggest-item"]}>
-              <img src="https://res.cloudinary.com/dayv9oa8q/image/upload/v1760368039/bulb_1_vlovvz.png" alt="suggest-icon" class=${styles["suggest-icon"]}/>
-              <div class=${styles["suggest-text"]}>How to hit the second plane with a tower</div>
-            </div>
+            <div class=${styles["auto-suggest-container"]} id="auto-suggest">
+              <div class=${styles["suggest-item"]}>
+                <img src="https://res.cloudinary.com/dayv9oa8q/image/upload/v1760368039/bulb_1_vlovvz.png" alt="suggest-icon" class=${styles["suggest-icon"]}/>
+                <div class=${styles["suggest-text"]}>How to be a racist?</div>
+              </div>
 
-            <div class=${styles["suggest-item"]}>
-              <img src="https://res.cloudinary.com/dayv9oa8q/image/upload/v1760368039/bulb_1_vlovvz.png" alt="suggest-icon" class=${styles["suggest-icon"]}/>
-              <div class=${styles["suggest-text"]}>Why Vince is racist?</div>
-            </div>
+              <div class=${styles["suggest-item"]}>
+                <img src="https://res.cloudinary.com/dayv9oa8q/image/upload/v1760368039/bulb_1_vlovvz.png" alt="suggest-icon" class=${styles["suggest-icon"]}/>
+                <div class=${styles["suggest-text"]}>How to hit the second plane with a tower</div>
+              </div>
 
-            <div class=${styles["suggest-item"]}>
-              <img src="https://res.cloudinary.com/dayv9oa8q/image/upload/v1760368039/bulb_1_vlovvz.png" alt="suggest-icon" class=${styles["suggest-icon"]}/>
-              <div class=${styles["suggest-text"]}>Why books are beautiful?</div>
-            </div>
+              <div class=${styles["suggest-item"]}>
+                <img src="https://res.cloudinary.com/dayv9oa8q/image/upload/v1760368039/bulb_1_vlovvz.png" alt="suggest-icon" class=${styles["suggest-icon"]}/>
+                <div class=${styles["suggest-text"]}>Why Vince is racist?</div>
+              </div>
 
-            <div class=${styles["suggest-item"]}>
-              <img src="https://res.cloudinary.com/dayv9oa8q/image/upload/v1760368039/bulb_1_vlovvz.png" alt="suggest-icon" class=${styles["suggest-icon"]}/>
-              <div class=${styles["suggest-text"]}>Why you gay?</div>
-            </div>
+              <div class=${styles["suggest-item"]}>
+                <img src="https://res.cloudinary.com/dayv9oa8q/image/upload/v1760368039/bulb_1_vlovvz.png" alt="suggest-icon" class=${styles["suggest-icon"]}/>
+                <div class=${styles["suggest-text"]}>Why books are beautiful?</div>
+              </div>
 
-            <div class=${styles["suggest-item"]}>
-              <img src="https://res.cloudinary.com/dayv9oa8q/image/upload/v1760368039/bulb_1_vlovvz.png" alt="suggest-icon" class=${styles["suggest-icon"]}/>
-              <div class=${styles["suggest-text"]}>Why hitler have a facebook account?</div>
+              <div class=${styles["suggest-item"]}>
+                <img src="https://res.cloudinary.com/dayv9oa8q/image/upload/v1760368039/bulb_1_vlovvz.png" alt="suggest-icon" class=${styles["suggest-icon"]}/>
+                <div class=${styles["suggest-text"]}>Why you gay?</div>
+              </div>
+
+              <div class=${styles["suggest-item"]}>
+                <img src="https://res.cloudinary.com/dayv9oa8q/image/upload/v1760368039/bulb_1_vlovvz.png" alt="suggest-icon" class=${styles["suggest-icon"]}/>
+                <div class=${styles["suggest-text"]}>Why hitler have a facebook account?</div>
+              </div>
             </div>
           </div>
-        </div>
 
-        <div class=${styles["header-navigations-container"]}>
-          <div class=${styles["selected"]} id=${styles["all-btn"]}>
-            <span>All</span>
-          </div>
-          <div class=${styles["not-selected"]} id="genres-btn">
-            <span>Genres</span>
-          </div>
-          <div class=${styles["not-selected"]} id="authors-btn">
-            <span>Authors</span>
-          </div>
-          <div class=${styles["not-selected"]} id="ratings-btn">
-            <span>Ratings</span>
-          </div>
-          <div class=${styles["not-selected"]} id="shoppings-btn">
-            <span>Shopping</span>
-          </div>
-          <div class=${styles["navigation-icon"]}>
-            <img src="https://res.cloudinary.com/dhisbk3b2/image/upload/v1760196138/book-spells_qetlpi.png" alt="">
+          <div class=${styles["header-navigations-container"]}>
+            <div class=${styles["selected"]} id=${styles["all-btn"]}>
+              <span>All</span>
+            </div>
+            <div class=${styles["not-selected"]} id="genres-btn">
+              <span>Genres</span>
+            </div>
+            <div class=${styles["not-selected"]} id="authors-btn">
+              <span>Authors</span>
+            </div>
+            <div class=${styles["not-selected"]} id="ratings-btn">
+              <span>Ratings</span>
+            </div>
+            <div class=${styles["not-selected"]} id="shoppings-btn">
+              <span>Shopping</span>
+            </div>
+            <div class=${styles["navigation-icon"]}>
+              <img src="https://res.cloudinary.com/dhisbk3b2/image/upload/v1760196138/book-spells_qetlpi.png" alt="">
+            </div>
           </div>
         </div>
       </div>
