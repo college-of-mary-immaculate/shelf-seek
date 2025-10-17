340a353ca93cc151258d2ddb7342062722d34af0
Haimonmon <beankor8@gmail.com>
2025-10-17 20:14:15 +0800

    Modified: lol

--- commands/nlp/levenshtein.py
+++ commands/nlp/levenshtein.py
@@ -4,14 +4,7 @@ data = bookbrains.FileManager().load_txt(r"data\lexicon\words.txt")
 
 query = "books about fictional and about author name ann liang"
 
-tokens = bookbrains.tokenizer(query)
-
-suggest = []
-for word in tokens:
-    result = bookbrains.levenshtein(word, choices = data)
-
-    if result:
-        suggest.append(result[0])
+suggest = [res[0] for word in  bookbrains.tokenizer(query) if (res := bookbrains.levenshtein(word, choices=data))]
 
 suggestion = " ".join(suggest)
 
