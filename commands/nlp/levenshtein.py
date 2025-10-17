888ac3b6f78fefb36cae93934cda9bff2514eb29
Haimonmon <beankor8@gmail.com>
2025-10-17 19:26:47 +0800

    Tested: auto correction

--- commands/nlp/levenshtein.py
+++ commands/nlp/levenshtein.py
@@ -2,4 +2,17 @@ import bookbrains
 
 data = bookbrains.FileManager().load_txt(r"data\lexicon\words.txt")
 
-leven = bookbrains.levenshtein("buxy", choices=data)
+query = "books about fictional and about author name ann liang"
+
+tokens = bookbrains.tokenizer(query)
+
+suggest = []
+for word in tokens:
+    result = bookbrains.levenshtein(word, choices = data)
+
+    if result:
+        suggest.append(result[0])
+
+suggestion = " ".join(suggest)
+
+print(suggestion)
