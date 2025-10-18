e388964b36fc433bc64d0b73248d2131ef106868
Haimonmon <beankor8@gmail.com>
2025-10-18 13:47:34 +0800

    gg

--- app/components/result/main.js
+++ app/components/result/main.js
@@ -3,12 +3,12 @@ import styles from './component.module.css';
 async function loadBookData(query) {
   try {
     console.log("ETO NAKUKUHA: ", query)
-    const response = await fetch("/barnesnobles.json");
+    const response = await fetch(`http://127.0.0.1:8000/api/v1/search?query=${query} `);
     const data = await response.json();
-
-    const listBook = data["books"]
+    console.log("Data", data)
+    const listBook = data["results"]
     
-    const firstTenBooks = listBook.slice(400, 420);
+    const firstTenBooks = listBook;
 
     console.log("10 sample books:", firstTenBooks);
     return firstTenBooks;
@@ -32,25 +32,25 @@ function formatDate(dateStr) {
 
 const createResultCard = (data) => {
     return `
-        <a href="${data.book.url}" target="_blank" class="${styles["book-link"]}">
+        <a href="${data.url}" target="_blank" class="${styles["book-link"]}">
             <div class=${styles["book-container"]}>
                 <div class=${styles["book-image-container"]}>
-                    <img src="${data.book.cover_image_url}" alt="">
+                    <img src="${data.cover_image_url}" alt="">
                 </div>
                 <div class=${styles["book-meta-container"]}>
                     <div class=${styles["meta-header"]}>
                         <div class=${styles["meta-title-container"]}>
-                            <span>${data.book.title}</span>
+                            <span>${data.title}</span>
                         </div>
                         <div class=${styles["meta-date-published-container"]}>
-                            <span>${formatDate(data.book.published_date)}</span>
+                            <span>${formatDate(data.published_date)}</span>
                         </div>
                     </div>
                     <div class=${styles["meta-description"]}>
-                        <span>${data.book.description}</span>
+                        <span>${data.description}</span>
                     </div>
                     <div class=${styles["meta-chips"]}>
-                        <div class=${styles["chip"]}><span>${data.author.name}</span></div>
+                        <div class=${styles["chip"]}><span>${data.name}</span></div>
                       
                         <div class=${styles["more-chip"]}>
                             <div class=${styles["bullet"]}></div>
@@ -83,10 +83,10 @@ const createBookShelfContainer = (data, num_stack = data.length) => {
 const createOtherAuthorResultCard = (data) => `
     <div class=${styles["author-container"]}>
         <div class="${styles["author-cover-image-container"]} ${data.author.image_cover_url ? styles["author-cover-image"] : styles["default1"]}">
-            <img src="${data.author.image_cover_url ? data.author.image_cover_url : "https://res.cloudinary.com/dhisbk3b2/image/upload/v1760286193/reading_1_lsukzn.png"}" alt="Adolf Hitler Cover Picture">
+            <img src="${data.image_cover_url ? data.image_cover_url : "https://res.cloudinary.com/dhisbk3b2/image/upload/v1760286193/reading_1_lsukzn.png"}" alt="Adolf Hitler Cover Picture">
         </div>
-        <h1>${data.author.name} </h1>
-        <span>${data.author.about} </span>
+        <h1>${data.name} </h1>
+        <span>${data.about} </span>
     </div>
 `;
 
