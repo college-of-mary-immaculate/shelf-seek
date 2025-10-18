eab760b69672686855bd6b5ec0a7b9786d805e01
Haimonmon <beankor8@gmail.com>
2025-10-18 13:22:49 +0800

    rsu

--- app/components/result/header.js
+++ app/components/result/header.js
@@ -5,17 +5,6 @@ export default function Header(root) {
     root.innerHTML = `
         <img src="https://res.cloudinary.com/dayv9oa8q/image/upload/v1759325005/SHELFSEEK_LOGO_g0ujec.png" alt="logo" class="${styles['icon-logo']}">
         <div class="${styles['top-container']}">
-<<<<<<< HEAD
-            <h1 class= "${styles['top-bar']}">
-                <div class= "${styles['search-bar']}">
-                    <input type="text" placeholder="Seek Books...">
-                </div>
-            </h1>
-            <h2 class= "${styles['nav-bar']}">
-                <img src="https://res.cloudinary.com/dayv9oa8q/image/upload/v1759325005/Group_111_bomakc.png" alt="nav" class= "${styles['nav-icon']}" id="navToggle">
-            </h2>
-            <div id="navRow" class= "${styles['nav-row']}">
-=======
             <h1 class="${styles['top-bar']}">
                 <div class="${styles['search-bar']}">
                     <input type="text" placeholder="Seek Books...">
@@ -25,7 +14,6 @@ export default function Header(root) {
                 <img src="https://res.cloudinary.com/dayv9oa8q/image/upload/v1759325005/Group_111_bomakc.png" alt="nav" class="${styles['nav-icon']}" id="navToggle">
             </h2>
             <div id="navRow" class="${styles['nav-row']}">
->>>>>>> c93c63235218fd27e00db828edbaa3bd6c11f98b
                 <h2><a href="#">All</a></h2>
                 <h2><a href="#">Genres</a></h2>
                 <h2><a href="#">Authors</a></h2>
@@ -35,10 +23,6 @@ export default function Header(root) {
         </div>
     `;
 
-<<<<<<< HEAD
-    // NOTE: If need ng css design ng header, kindly add the css to the component.module.css and uncomment this
-    root.className = styles['header'] || '';
-=======
     root.className = styles['header'];
 
     const navToggle = document.getElementById('navToggle');
@@ -47,5 +31,4 @@ export default function Header(root) {
     navToggle.addEventListener('click', function() {
         navRow.classList.toggle('show');
     });
->>>>>>> c93c63235218fd27e00db828edbaa3bd6c11f98b
 }
--- app/components/result/main.js
+++ app/components/result/main.js
@@ -2,15 +2,6 @@ import styles from './component.module.css';
 
 export default function Main(root) {
     root.innerHTML = `
-<<<<<<< HEAD
-        <div class= ${styles['book-container']}>
-            <div class= ${styles['dit-1']}>
-                <img src="https://res.cloudinary.com/dayv9oa8q/image/upload/v1759490350/9781408855669-6cfb2099b6e84a4899ce368d6facc242_4_2_u6cxxp.png" alt="b-1" class= ${styles['b-1']}>
-                <div class= ${styles['middle-con-1']}>
-                    <h1 class= ${styles['title-1']}>Harry Potter</h1>
-                    <h2 class= ${styles['mini-detail-1']}>The Chamber of Secrets<span class= ${styles['date-1']}>October 3, 2025</span></h2>
-                    <h3 class= ${styles['info-1']}>
-=======
         <div class="${styles['book-container']}">
             <div class="${styles['dit-1']}">
                 <img src="https://res.cloudinary.com/dayv9oa8q/image/upload/v1759490350/9781408855669-6cfb2099b6e84a4899ce368d6facc242_4_2_u6cxxp.png" alt="b-1" class="${styles['b-1']}">
@@ -18,7 +9,6 @@ export default function Main(root) {
                     <h1 class="${styles['title-1']}">Harry Potter</h1>
                     <h2 class="${styles['mini-detail-1']}">The Chamber of Secrets<span class="${styles['date-1']}">October 3, 2025</span></h2>
                     <h3 class="${styles['info-1']}">
->>>>>>> c93c63235218fd27e00db828edbaa3bd6c11f98b
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do 
                         eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut 
                         enim ad minim veniam, quis nostrud exercitation ullamco laboris 
@@ -26,22 +16,6 @@ export default function Main(root) {
                         reprehenderit in voluptate velit esse cillum dolore eu fugiat 
                         null...
                     </h3>
-<<<<<<< HEAD
-                    <div class= ${styles['button-container']}>
-                        <button class= ${styles['btns-1']}>J.K Rowling</button>
-                        <button class= ${styles['btns-1']}>J.K Rowling</button>
-                        <button class= ${styles['btns-1']}>J.K Rowling</button>
-                        <button class= ${styles['btns-1']}><img src="https://res.cloudinary.com/dayv9oa8q/image/upload/v1759501467/Group_31_uo29p9.png" alt="dots"></button>
-                    </div>
-                </div>
-            </div>
-            <div class= ${styles['dit-2']}>
-                <img src="https://res.cloudinary.com/dayv9oa8q/image/upload/v1759334330/images_3_4_ml4upo.png" alt="b-2" class= ${styles['b-2']}>
-                <div class= ${styles['middle-con-2']}>
-                    <h1 class= ${styles['title-2']}>Harry Potter</h1>
-                    <h2 class= ${styles['mini-detail-2']}>The Chamber of Secrets<span class= ${styles['date-2']}>October 3, 2025</span></h2>
-                    <h3 class= ${styles['info-2']}>
-=======
                     <div class="${styles['button-container']}">
                         <button class="${styles['btns-1']}">J.K Rowling</button>
                         <button class="${styles['btns-1']}">J.K Rowling</button>
@@ -56,7 +30,6 @@ export default function Main(root) {
                     <h1 class="${styles['title-2']}">Harry Potter</h1>
                     <h2 class="${styles['mini-detail-2']}">The Chamber of Secrets<span class="${styles['date-2']}">October 3, 2025</span></h2>
                     <h3 class="${styles['info-2']}">
->>>>>>> c93c63235218fd27e00db828edbaa3bd6c11f98b
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do 
                         eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut 
                         enim ad minim veniam, quis nostrud exercitation ullamco laboris 
@@ -64,22 +37,6 @@ export default function Main(root) {
                         reprehenderit in voluptate velit esse cillum dolore eu fugiat 
                         null...
                     </h3>
-<<<<<<< HEAD
-                    <div class= ${styles['button-container']}>
-                        <button class= ${styles['btns-2']}>J.K Rowling</button>
-                        <button class= ${styles['btns-2']}>J.K Rowling</button>
-                        <button class= ${styles['btns-2']}>J.K Rowling</button>
-                        <button class= ${styles['btns-2']}><img src="https://res.cloudinary.com/dayv9oa8q/image/upload/v1759501467/Group_31_uo29p9.png" alt="dots"></button>
-                    </div>
-                </div>
-            </div>
-            <div class= ${styles['dit-3']}>
-                <img src="https://res.cloudinary.com/dayv9oa8q/image/upload/v1759334330/images_1_5_ogrcsl.png" alt="b-3" class= ${styles['b-3']}>
-                <div class= ${styles['middle-con-3']}>
-                    <h1 class= ${styles['title-3']}>Harry Potter</h1>
-                    <h2 class= ${styles['mini-detail-3']}>The Chamber of Secrets<span class="date-3">October 3, 2025</span></h2>
-                    <h3 class= ${styles['info-3']}>
-=======
                     <div class="${styles['button-container']}">
                         <button class="${styles['btns-2']}">J.K Rowling</button>
                         <button class="${styles['btns-2']}">J.K Rowling</button>
@@ -94,7 +51,6 @@ export default function Main(root) {
                     <h1 class="${styles['title-3']}">Harry Potter</h1>
                     <h2 class="${styles['mini-detail-3']}">The Chamber of Secrets<span class="${styles['date-3']}">October 3, 2025</span></h2>
                     <h3 class="${styles['info-3']}">
->>>>>>> c93c63235218fd27e00db828edbaa3bd6c11f98b
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do 
                         eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut 
                         enim ad minim veniam, quis nostrud exercitation ullamco laboris 
@@ -102,20 +58,6 @@ export default function Main(root) {
                         reprehenderit in voluptate velit esse cillum dolore eu fugiat 
                         null...
                     </h3>
-<<<<<<< HEAD
-                    <div class= ${styles['button-container']}>
-                        <button class= ${styles['btns-3']}>J.K Rowling</button>
-                        <button class= ${styles['btns-3']}>J.K Rowling</button>
-                        <button class= ${styles['btns-3']}>J.K Rowling</button>
-                        <button class= ${styles['btns-3']}><img src="https://res.cloudinary.com/dayv9oa8q/image/upload/v1759501467/Group_31_uo29p9.png" alt="dots"></button>
-            </div>
-        </div>
-        <div class= ${styles['author-container']}>
-            <img src="https://res.cloudinary.com/dayv9oa8q/image/upload/v1759324992/Group_112_ruf576.png" alt="a-icon">
-            <header class= ${styles['author-name']}>J.K Rowling</header>
-            <h3 class=${styles['author-description']}>British author and philanthropist (born 1965)</h3>
-            <img src="https://res.cloudinary.com/dayv9oa8q/image/upload/v1759324980/JK-Rowling1_1_mvo2nt.png" alt="author-pic" class= ${styles['author-pic']}>
-=======
                     <div class="${styles['button-container']}">
                         <button class="${styles['btns-3']}">J.K Rowling</button>
                         <button class="${styles['btns-3']}">J.K Rowling</button>
@@ -130,7 +72,6 @@ export default function Main(root) {
             <header class="${styles['author-name']}">J.K Rowling</header>
             <h3 class="${styles['author-description']}">British author and philanthropist (born 1965)</h3>
             <img src="https://res.cloudinary.com/dayv9oa8q/image/upload/v1759324980/JK-Rowling1_1_mvo2nt.png" alt="author-pic" class="${styles['author-pic']}">
->>>>>>> c93c63235218fd27e00db828edbaa3bd6c11f98b
         </div>
     `;
 
