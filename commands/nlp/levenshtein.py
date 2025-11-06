3ae6828e2e49d30092ef81d37b1c04abaf7d4188
rey <reyarby.castro@gmail.com>
2025-11-06 18:42:53 +0800

    added changes in levenshtein

--- commands/nlp/levenshtein.py
+++ commands/nlp/levenshtein.py
@@ -2,7 +2,7 @@ import bookbrains
 
 data = bookbrains.FileManager().load_txt(r"data\lexicon\words.txt")
 
-query = "books about fictional and about author name ann liang"
+query = "books about fictional and about author name ann ling"
 
 suggest = [res[0] for word in  bookbrains.tokenizer(query) if (res := bookbrains.levenshtein(word, choices=data))]
 
